f531923849bb88041053946a6785d28e81c5e726
avez07 <ansariaavez12345@gmail.com>
2025-08-06 17:39:52 +0530

    tracking added

--- src/Models/Gati.js
+++ src/Models/Gati.js
@@ -57,7 +57,7 @@ const Gatitracking = ModalErrorHandller(async (awb) => {
     
     
     // console.log(row)
-    const testUrl = "https://pg-uat.gati.com/pickupservices/GKEJCustVendDtls.jsp"
+    const testUrl = `https://pg-uat.gati.com/pickupservices/GatiKWEDktJTrack.jsp?p1=${awb}&p2=53A0A0644DC94C25`
     const ProductionUrl = `https://justi.com/pickupservices/GatiKWEDktJTrack.jsp?p1=${awb}&p2=53A0A0644DC94C25`
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
